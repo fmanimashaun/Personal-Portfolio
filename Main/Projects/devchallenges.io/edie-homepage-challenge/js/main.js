8095bec00a07ffbf49c19230ce73d71589245cc6
fmanimashaun <fisayo.animashaun@outlook.com>
2022-08-14 10:29:13 +0100

    updated setup

---
+++ Main/Projects/devchallenges.io/edie-homepage-challenge/js/main.js
@@ -0,0 +1,30 @@
+//Toggle search bar for mobile
+
+//create variables
+const menuButton = document.querySelector('.nav__menu');
+const closeButton = document.querySelector('.close__icon');
+const mobileMenu = document.querySelector('.mobile__menu');
+const bodyWidth = window.innerWidth;
+
+
+//add event listener for menu button 
+menuButton.addEventListener('click', function () {
+    mobileMenu.style.display = 'block';
+});
+
+
+//add event listener for close button
+closeButton.addEventListener('click', function () {
+    mobileMenu.style.display = 'none';
+});
+
+/*
+//restore the menu when the window is resized
+window.addEventListener("resize", function() {
+    if(bodyWidth < 820){
+        mobileMenu.style.display = 'block';
+    } else {
+        mobileMenu.style.display = 'none';
+    }
+});
+*/
