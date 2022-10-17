4f2d7a78da2a260813370b6402cef75267ade6e9
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-10-17 21:43:26 +0100

    Update projeect files

--- linearly_scale_font-size_with_clamp.js
+++ linearly_scale_font-size_with_clamp.js
@@ -1,13 +1,15 @@
 // Takes the viewport widths in pixels and the font sizes in rem
-function clampBuilder( minWidthPx, maxWidthPx, minFontSize, maxFontSize ) {
-  const root = document.querySelector( "html" );
-  const pixelsPerRem = Number( getComputedStyle( root ).fontSize.slice( 0,-2 ) );
+function clampBuilder (minWidthPx, maxWidthPx, minFontSize, maxFontSize) {
+  const root = document.querySelector("html");
+  const pixelsPerRem = Number(getComputedStyle(root).fontSize.slice(0, -2));
 
   const minWidth = minWidthPx / pixelsPerRem;
   const maxWidth = maxWidthPx / pixelsPerRem;
 
-  const slope = ( maxFontSize - minFontSize ) / ( maxWidth - minWidth );
+  const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth);
   const yAxisIntersection = -minWidth * slope + minFontSize
 
-  return `clamp( ${ minFontSize }rem, ${ yAxisIntersection }rem + ${ slope * 100 }vw, ${ maxFontSize }rem )`;
+  return `clamp( ${minFontSize}rem, ${yAxisIntersection}rem + ${slope * 100}vw, ${maxFontSize}rem )`;
 }
+
+console.log(clampBuilder(375, 1440, 28, 32));
