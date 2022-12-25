15552bd5d3a546c9c6f4acd8b36b3b99666e4bcc
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-25 21:34:59 +0100

    Updated

--- Main/Projects/scrimba_project/basketball-scoreboard/js/main.js
+++ Main/Projects/scrimba_project/basketball-scoreboard/js/main.js
@@ -1,3 +1,4 @@
 // Create a timer that runs from 0 to 12 minutes
 // at the end of the timer, score of the highest should be blinking with a color change to green
 // if the score is same, the color should be yellow for both team and the score should blink
+// add Confetti Falling effect when the timer ends
