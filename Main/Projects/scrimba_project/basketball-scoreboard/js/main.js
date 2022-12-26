86a644278230cce3f06e7d36dbd2c3d24e8a55b1
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-26 18:11:43 +0100

    updated

--- Main/Projects/scrimba_project/basketball-scoreboard/js/main.js
+++ Main/Projects/scrimba_project/basketball-scoreboard/js/main.js
@@ -1,3 +1,10 @@
+// get all elemets from DOM
+
+const clock = document.querySelector('.timer-content');
+
+const homeTeamScoreBtns = document.querySelector('.home__scoring-btn');
+const homeTeamScore = document.querySelector('#score_home');
+
 // Create a timer that runs from 0 to 12 minutes
 // at the end of the timer, score of the highest should be blinking with a color change to green
 // if the score is same, the color should be yellow for both team and the score should blink
